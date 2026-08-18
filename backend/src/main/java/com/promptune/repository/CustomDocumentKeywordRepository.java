cf426121e681e2fdc1e1c05a6fa0c0808f07fe33
이형기 <lhkkd1028@gmail.com>
2026-08-18 13:23:25 +0900

    chore: custom_document_keyword 삭제 + feat: 요소별 적용/거절 기록 API

--- backend/src/main/java/com/promptune/repository/CustomDocumentKeywordRepository.java
+++
@@ -1,9 +0,0 @@
-package com.promptune.repository;
-
-import com.promptune.domain.CustomDocumentKeyword;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface CustomDocumentKeywordRepository extends JpaRepository<CustomDocumentKeyword, Long> {
-    List<CustomDocumentKeyword> findByCompanyId(String companyId);
-}
